c22868e3cb09392478b7aa371a8050ac30a3357a
Lurius-Kitsune <LucasFox@outlook.fr>
2023-12-12 22:46:53 +0100

    - Added Movement Controll on the car
    - Added challenge 1 for the next of the parcours

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float SpeedOfVehicle;
+    private float SpeedOfVehicle = 20.0f;
+    private float turnSpeed = 100.0f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Log Time.deltaTime
-        transform.Translate(Vector3.forward * Time.deltaTime * this.SpeedOfVehicle);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Mives the car forward based on vertical Input
+        transform.Translate(Vector3.forward * Time.deltaTime * this.SpeedOfVehicle * verticalInput);
+        // If there is vertical Input we can rotate
+        // the car based on Horizontal Input
+        if (verticalInput != 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * this.turnSpeed * horizontalInput);
+        }
     }
 }
