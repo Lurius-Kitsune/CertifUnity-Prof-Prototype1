bd44822843248b8bd5252917bdd8fbaf0bf17aef
Lurius-Kitsune <LucasFox@outlook.fr>
2023-12-12 16:45:01 +0100

    - Added speed controller for wehicle

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float SpeedOfVehicle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Log Time.deltaTime
+        transform.Translate(Vector3.forward * Time.deltaTime * this.SpeedOfVehicle);
+    }
+}
