781dbba6b9d2bc23cb8f31f1263f6cb21fbee6ec
Lurius-Kitsune <LucasFox@outlook.fr>
2023-12-12 16:53:18 +0100

    - Camera follow the player

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleFly : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,8 @@ public class ObstacleFly : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + this.offset;
     }
 }
