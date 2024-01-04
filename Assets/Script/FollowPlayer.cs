88e1b098b98d3bde3f8474891645f76d50a19ed0
Lurius-Kitsune <LucasFox@outlook.fr>
2024-01-04 14:10:05 +0100

    Added IA, Added Back camera on click

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    public GameObject backCam;
+    private bool cameraToogle;
     public Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 backCamPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position;
+        backCamPos = backCam.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        cameraToogle = Input.GetButton("Fire1");
+        switch (cameraToogle)
+        {
+            case false:
+                backCam.SetActive(false);
+                break;
+            case true:
+                backCam.SetActive(true);
+                break;
+        }
         transform.position = player.transform.position + this.offset;
+        backCam.transform.position = player.transform.position + this.backCamPos;
     }
 }
